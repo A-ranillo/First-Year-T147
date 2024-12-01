60319ffbedde811a554f78e168e775ac0bd9e957
A-ranillo <ranillo.philipbezalelasher@gmail.com>
2024-12-01 13:20:22 -0500

    Delete comp2155/assignments directory

--- comp2155/assignments/mini-assigment 4/task2.py
+++
@@ -1,5 +0,0 @@
-with open(f"ospf_routing_protocol.txt", "w") as f:
-    f.write(f"router ospf 1")
-    f.write(f"0.0.0.0 0.0.0.0 area 0")
-    f.write(f"110")
-    f.write(f"default-information originate always")
